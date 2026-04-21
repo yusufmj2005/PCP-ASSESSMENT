dc2eb53f930711ce5b698487a59340d0c0e33c25
Yusuf <e0423018@sriher.edu.in>
2026-04-21 09:55:58 +0530

    add validation logic

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,18 +1,22 @@
 const AppReducer = (state, action) => {
     switch (action.type) {
         case "SET_DATA":
-            const cleaned = action.payload.filter(
-                (item) =>
+            const validData = (action.payload || []).filter((item) => {
+                return (
                     item &&
                     item.ActivityId !== undefined &&
                     item.name &&
+                    Number(item.steps) > 0 &&
+                    Number(item.caloriesBurned) > 0 &&
+                    Number(item.workoutMinutes) > 0 &&
                     typeof item.goalAchieved === "boolean"
-            );
+                );
+            });
 
             return {
                 ...state,
-                data: cleaned,
-                filtered: cleaned
+                data: validData,
+                filtered: validData
             };
 
         case "FILTER":
